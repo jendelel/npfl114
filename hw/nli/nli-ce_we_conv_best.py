4b94f0ce983a3f9fbf1b7304bf3f48c08b2d4852
Ondrej Skopek <skopekondrej@gmail.com>
2016-12-29 22:57:04 +0100

    Added best model (~75% in 23 epochs)

--- hw/nli/nli-ce_we_conv_best.py
+++ hw/nli/nli-ce_we_conv_best.py
@@ -38,7 +38,7 @@ class Network:
             #print("Conv:", conv_2.get_shape())
             return conv_1
 
-    def __init__(self, rnn_cell, rnn_cell_dim, num_words, num_chars, logdir, expname, threads=1, seed=42, word_embedding=100, char_embedding=100, keep_prob=0.5, num_filters=512):
+    def __init__(self, rnn_cell, rnn_cell_dim, num_words, num_chars, logdir, expname, threads=1, seed=42, word_embedding=100, char_embedding=100, keep_prob=0.5, num_filters=512, l2=0.001):
         # Create an empty graph and a session
         graph = tf.Graph()
         graph.seed = seed
@@ -50,94 +50,95 @@ class Network:
 
         # Construct the graph
         with self.session.graph.as_default():
-            if rnn_cell == "LSTM":
-                rnn_cell_co = tf.nn.rnn_cell.LSTMCell(rnn_cell_dim)
-            elif rnn_cell == "GRU":
-                rnn_cell_co = tf.nn.rnn_cell.GRUCell(rnn_cell_dim)
-            else:
-                raise ValueError("Unknown rnn_cell {}".format(rnn_cell))
+            with tf.variable_scope("l2", regularizer=tf_layers.l2_regularizer(l2)):
+                if rnn_cell == "LSTM":
+                    rnn_cell_co = tf.nn.rnn_cell.LSTMCell(rnn_cell_dim)
+                elif rnn_cell == "GRU":
+                    rnn_cell_co = tf.nn.rnn_cell.GRUCell(rnn_cell_dim)
+                else:
+                    raise ValueError("Unknown rnn_cell {}".format(rnn_cell))
 
-            self.global_step = tf.Variable(0, dtype=tf.int64, trainable=False, name="global_step")
-            self.sentence_lens = tf.placeholder(tf.int32, [None])
-            self.word_ids = tf.placeholder(tf.int32, [None, None])
-            self.charseq_ids = tf.placeholder(tf.int32, [None, None])
-            self.charseqs = tf.placeholder(tf.int32, [None, None])
-            self.charseq_lens = tf.placeholder(tf.int32, [None])
-            self.languages = tf.placeholder(tf.int32, [None])
-            self.is_training = tf.placeholder_with_default(False, [])
+                self.global_step = tf.Variable(0, dtype=tf.int64, trainable=False, name="global_step")
+                self.sentence_lens = tf.placeholder(tf.int32, [None])
+                self.word_ids = tf.placeholder(tf.int32, [None, None])
+                self.charseq_ids = tf.placeholder(tf.int32, [None, None])
+                self.charseqs = tf.placeholder(tf.int32, [None, None])
+                self.charseq_lens = tf.placeholder(tf.int32, [None])
+                self.languages = tf.placeholder(tf.int32, [None])
+                self.is_training = tf.placeholder_with_default(False, [])
 
-            if char_embedding == -1:
-                input_chars = tf.one_hot(self.charseqs, num_chars)
-            else:
-                input_chars = tf.nn.embedding_lookup(tf.get_variable("char_emb", shape=[num_chars, char_embedding]),
-                                                     self.charseqs)
-            print("input_chars", input_chars.get_shape())
+                if char_embedding == -1:
+                    input_chars = tf.one_hot(self.charseqs, num_chars)
+                else:
+                    input_chars = tf.nn.embedding_lookup(tf.get_variable("char_emb", shape=[num_chars, char_embedding]),
+                                                         self.charseqs)
+                print("input_chars", input_chars.get_shape())
 
-            if rnn_cell == "LSTM":
-                rnn_cell_ce = tf.nn.rnn_cell.LSTMCell(rnn_cell_dim)
-            elif rnn_cell == "GRU":
-                rnn_cell_ce = tf.nn.rnn_cell.GRUCell(rnn_cell_dim)
-            else:
-                raise ValueError("Unknown rnn_cell {}".format(rnn_cell))
-            _, (state_fw, state_bw) = tf.nn.bidirectional_dynamic_rnn(rnn_cell_ce, rnn_cell_ce, input_chars,
-                                                                          self.charseq_lens, dtype=tf.float32, scope="rnn_chars")
-            input_chars = state_fw + state_bw
-            print("input_chars", input_chars.get_shape())
+                if rnn_cell == "LSTM":
+                    rnn_cell_ce = tf.nn.rnn_cell.LSTMCell(rnn_cell_dim)
+                elif rnn_cell == "GRU":
+                    rnn_cell_ce = tf.nn.rnn_cell.GRUCell(rnn_cell_dim)
+                else:
+                    raise ValueError("Unknown rnn_cell {}".format(rnn_cell))
+                _, (state_fw, state_bw) = tf.nn.bidirectional_dynamic_rnn(rnn_cell_ce, rnn_cell_ce, input_chars,
+                                                                              self.charseq_lens, dtype=tf.float32, scope="rnn_chars")
+                input_chars = state_fw + state_bw
+                print("input_chars", input_chars.get_shape())
 
-            input_char_words = tf.nn.embedding_lookup(input_chars, self.charseq_ids)
-            print("input_char_words", input_char_words.get_shape())
+                input_char_words = tf.nn.embedding_lookup(input_chars, self.charseq_ids)
+                print("input_char_words", input_char_words.get_shape())
 
-            if word_embedding == -1:
-                input_words = tf.one_hot(self.word_ids, num_words)
-            else:
-                input_words = tf.nn.embedding_lookup(tf.get_variable("word_emb", shape=[num_words, word_embedding]),
-                                                     self.word_ids)
-            print("input_words", input_words.get_shape())
-            inputs = tf.pack([input_char_words, input_words], axis=3)
-            print("inputs", inputs.get_shape())
-
-
-            seq_len = 250
-            sliced = tf.slice(inputs, [0, 0, 0, 0], [-1, seq_len, char_embedding, 2])
-
-            c3 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[3, char_embedding], stride=1)
-            print("c3", c3.get_shape())
-            c4 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[4,char_embedding], stride=1)
-            print("c4", c4.get_shape())
-            c5 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[5,char_embedding], stride=1)
-            print("c5", c5.get_shape())
-            c7 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[7,char_embedding], stride=1)
-            print("c7", c7.get_shape())
-
-            pooled = []
-            mp = tf_layers.max_pool2d(inputs=c3, kernel_size=[seq_len - 3 + 1, 1], stride=1)
-            print("pool", mp.get_shape())
-            pooled.append(mp)
-            pooled.append(tf_layers.max_pool2d(inputs=c4, kernel_size=[seq_len - 4 + 1, 1], stride=1))
-            pooled.append(tf_layers.max_pool2d(inputs=c5, kernel_size=[seq_len - 5 + 1, 1], stride=1))
-            pooled.append(tf_layers.max_pool2d(inputs=c7, kernel_size=[seq_len - 7 + 1, 1], stride=1))
+                if word_embedding == -1:
+                    input_words = tf.one_hot(self.word_ids, num_words)
+                else:
+                    input_words = tf.nn.embedding_lookup(tf.get_variable("word_emb", shape=[num_words, word_embedding]),
+                                                         self.word_ids)
+                print("input_words", input_words.get_shape())
+                inputs = tf.pack([input_char_words, input_words], axis=3)
+                print("inputs", inputs.get_shape())
 
 
-            pooled_outputs = tf.concat(3, pooled)
-            print("pooled_outputs", pooled_outputs.get_shape())
-            flatten = tf.reshape(pooled_outputs, [-1, 4 * num_filters])
-            print("flatten", flatten.get_shape())
-            dropout = tf_layers.dropout(flatten, keep_prob=keep_prob, is_training=self.is_training)
+                seq_len = 250
+                sliced = tf.slice(inputs, [0, 0, 0, 0], [-1, seq_len, char_embedding, 2])
 
-            # dec_outputs, _ = tf.nn.seq2seq.attention_decoder([dec_inputs], state_fw + state_bw, outputs, rnn_cell_co,
-            #                                                  loop_function=loop_fn, output_size=self.LANGUAGES)
-            output_layer = tf_layers.fully_connected(dropout, num_outputs=self.LANGUAGES)
-            self.loss = loss = tf_losses.sparse_softmax_cross_entropy(output_layer, self.languages)
-            self.training = tf.train.AdamOptimizer(1e-4).minimize(loss, self.global_step)
-            self.predictions = tf.cast(tf.argmax(output_layer, 1), tf.int32)
-            self.accuracy = tf_metrics.accuracy(self.predictions, self.languages)
+                c3 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[3, char_embedding], stride=1)
+                print("c3", c3.get_shape())
+                c4 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[4,char_embedding], stride=1)
+                print("c4", c4.get_shape())
+                c5 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[5,char_embedding], stride=1)
+                print("c5", c5.get_shape())
+                c7 = self._1d_conv(sliced, num_outputs=num_filters, kernel_size=[7,char_embedding], stride=1)
+                print("c7", c7.get_shape())
 
-            self.dataset_name = tf.placeholder(tf.string, [])
-            self.summary = tf.merge_summary([tf.scalar_summary(self.dataset_name+"/loss", loss),
-                                             tf.scalar_summary(self.dataset_name+"/accuracy", self.accuracy)])
+                pooled = []
+                mp = tf_layers.max_pool2d(inputs=c3, kernel_size=[seq_len - 3 + 1, 1], stride=1)
+                print("pool", mp.get_shape())
+                pooled.append(mp)
+                pooled.append(tf_layers.max_pool2d(inputs=c4, kernel_size=[seq_len - 4 + 1, 1], stride=1))
+                pooled.append(tf_layers.max_pool2d(inputs=c5, kernel_size=[seq_len - 5 + 1, 1], stride=1))
+                pooled.append(tf_layers.max_pool2d(inputs=c7, kernel_size=[seq_len - 7 + 1, 1], stride=1))
 
-            # Initialize variables
-            self.session.run(tf.initialize_all_variables())
+
+                pooled_outputs = tf.concat(3, pooled)
+                print("pooled_outputs", pooled_outputs.get_shape())
+                flatten = tf.reshape(pooled_outputs, [-1, 4 * num_filters])
+                print("flatten", flatten.get_shape())
+                dropout = tf_layers.dropout(flatten, keep_prob=keep_prob, is_training=self.is_training)
+
+                # dec_outputs, _ = tf.nn.seq2seq.attention_decoder([dec_inputs], state_fw + state_bw, outputs, rnn_cell_co,
+                #                                                  loop_function=loop_fn, output_size=self.LANGUAGES)
+                output_layer = tf_layers.fully_connected(dropout, num_outputs=self.LANGUAGES)
+                self.loss = loss = tf_losses.sparse_softmax_cross_entropy(output_layer, self.languages)
+                self.training = tf.train.AdamOptimizer(1e-4).minimize(loss, self.global_step)
+                self.predictions = tf.cast(tf.argmax(output_layer, 1), tf.int32)
+                self.accuracy = tf_metrics.accuracy(self.predictions, self.languages)
+
+                self.dataset_name = tf.placeholder(tf.string, [])
+                self.summary = tf.merge_summary([tf.scalar_summary(self.dataset_name+"/loss", loss),
+                                                 tf.scalar_summary(self.dataset_name+"/accuracy", self.accuracy)])
+
+                # Initialize variables
+                self.session.run(tf.initialize_all_variables())
 
     @property
     def training_step(self):
@@ -177,7 +178,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_train", default="nli-dataset/nli-train.txt", type=str, help="Training data file.")
     parser.add_argument("--data_dev", default="nli-dataset/nli-dev.txt", type=str, help="Development data file.")
     parser.add_argument("--data_test", default="nli-dataset/nli-test.txt", type=str, help="Testing data file.")
-    parser.add_argument("--epochs", default=25, type=int, help="Number of epochs.")
+    parser.add_argument("--epochs", default=23, type=int, help="Number of epochs.")
     parser.add_argument("--logdir", default="logs", type=str, help="Logdir name.")
     parser.add_argument("--rnn_cell", default="GRU", type=str, help="RNN cell type.")
     parser.add_argument("--rnn_cell_dim", default=200, type=int, help="RNN cell dimension.")
@@ -186,6 +187,7 @@ if __name__ == "__main__":
     parser.add_argument("--char_embedding", default=200, type=int, help="char_embedding")
     parser.add_argument("--keep_prob", default=0.5, type=float, help="dropout probability")
     parser.add_argument("--num_filters", default=1024, type=int, help="number of output filters from convolution")
+    parser.add_argument("--l2", default=0.001, type=float, help="l2 lambda")
 
     args = parser.parse_args()
 
@@ -197,12 +199,13 @@ if __name__ == "__main__":
 
     # Construct the network
     print("Constructing the network.", file=sys.stderr)
-    expname = "{}-{}{}-bs{}-epochs{}-char{}-word{}".format(tools.exp_name(__file__), args.rnn_cell, args.rnn_cell_dim, args.batch_size, args.epochs, args.char_embedding, args.word_embedding)
+    expname = "{}-{}{}-bs{}-epochs{}-char{}-word{}-nf{}-l2:{}".format(tools.exp_name(__file__), args.rnn_cell, args.rnn_cell_dim, args.batch_size, args.epochs, args.char_embedding, args.word_embedding, args.num_filters, args.l2)
     network = Network(rnn_cell=args.rnn_cell, rnn_cell_dim=args.rnn_cell_dim,
                       num_words=len(data_train.vocabulary('words')), num_chars=len(data_train.vocabulary('chars')),
                       logdir=args.logdir, expname=expname, threads=args.threads,
                       word_embedding=args.word_embedding, char_embedding=args.char_embedding,
-                      keep_prob=args.keep_prob, num_filters=args.num_filters)
+                      keep_prob=args.keep_prob, num_filters=args.num_filters, 
+                      l2=args.l2)
 
     # Train
     best_dev_accuracy = 0
@@ -227,6 +230,6 @@ if __name__ == "__main__":
             test_predictions = network.predict(sentence_lens, word_ids, charseq_ids, charseqs, charseq_lens)
 
     # Print test predictions
-    with open("labels.txt", "w") as f:
+    with open("labels_best.txt", "w") as f:
         for prediction in test_predictions:
             print(data_test.vocabulary('languages')[prediction], file=f)
